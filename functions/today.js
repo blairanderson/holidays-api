fd750423ea2c66ad9e621d139069a3384835de24
Blair Anderson <blair81@gmail.com>
2021-12-27 13:08:42 -0800

    today.js

--- functions/today.js
+++ functions/today.js
@@ -2,6 +2,7 @@ const data = require("../public/holiday-data.json")
 const {filterDates} = require("../filterDates.js");
 
 const filterDate = "observedOn";
+
 exports.handler = async (event, context) => {
   const padding = validPadding(event.queryStringParameters.padding) || 7;
   const today = filterDates(data.holidays, filterDate, padding);
@@ -11,5 +12,5 @@ exports.handler = async (event, context) => {
 
 function validPadding(param){
   const v = parseInt(param)
-  return v > 0 && && v < 365 && v;
+  return v > 0 && v < 365 && v;
 }
