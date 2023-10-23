b52fac3b798409e25cab849123ba47a87d42647c
Blair Anderson <blair81@gmail.com>
2023-10-23 09:11:19 -0700

    require api key

--- functions/holidays.js
+++ functions/holidays.js
@@ -1,5 +1,9 @@
 const data = require("../public/holiday-data.json")
 
 exports.handler = async (event, context) => {
-  return { statusCode: 200, body: JSON.stringify(data) };
+  if (process.env.API_KEY === event.queryStringParameters.api_key) {
+    return { statusCode: 200, body: JSON.stringify(data) };
+  } else {
+    return { statusCode: 429, body: JSON.stringify({message: "Please host your own version of this API https://github.com/blairanderson/holidays-api/"}) };
+  }
 };
--- today.js
+++
@@ -1,9 +0,0 @@
-// node today.js
-const data = require("./public/holiday-data.json")
-const {filterDates} = require("./filterDates.js");
-
-const filterDate = "observedOn";
-const padding = 7
-const today = filterDates(data.holidays, filterDate, padding)
-
-console.log(JSON.stringify({today, filterDate, padding}, null, 4))
